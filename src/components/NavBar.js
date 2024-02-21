24be67afc69241ab4b5983b5a56425fba783ae37
NicolasM <nicolasmahecha11@hotmail.com>
2024-02-21 16:23:20 -0500

    config url mail final funciono email netlify

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -35,7 +35,7 @@ export const NavBar = () => {
       <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
         <Container>
           <Navbar.Brand href="/">
-            <img src={logo1} alt="Logo" />
+            <img className="logo" src={logo1} alt="Logo" />
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav">
             <span className="navbar-toggler-icon"></span>
